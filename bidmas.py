db6709dfe08466b6a98120c17016db1a9d130f93
salahm aleh kum <10jknight@ashmanorstudent.com>
2014-03-05 10:04:31 +0000

    Create bidmas.py

---
+++ bidmas.py
@@ -0,0 +1,14 @@
+def bidmas1 (fred):
+    fred = fred*3
+    fred = fred+4
+    return (fred)
+    
+def bidmas2 (fred):
+    fred = fred+4
+    fred = fred*3
+    return (fred)
+    
+print(bidmas1 (5))
+#exp 19
+print (bidmas2 (6))
+#exp 30
